8aa69abecb78813ca2fba0dca5e43760a09dae7b
MARSAD MANZOOR <2022CS203@student.uet.edu.pk>
2026-01-09 21:17:06 +0500

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,11 +1,16 @@
-def main():
-    num = int(input("Enter a number: "))
-    
-    if num % 2 == 0:
-        print("The number is even")
-    else:
-        print("The number is odd")
-   
-if __name__ == "__main__":
+num = int(input("Enter a number: "))
 
-    main()
+if num <= 1:
+    print("Not a prime number")
+else:
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime number")
+    else:
+        print("Not a prime number")
+
